80c9e366c7c03006b4abffea181f88d0b3076d25
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-12-03 09:21:13 +0100

    setup plugins

---
+++ dynamic-visual-categories.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Dynamic Visual Categories
+Plugin URI: https://yourwebsite.com/
+Description: A dynamic dropdown menu that displays images based on the selected category.
+Version: 1.0
+Author: Your Name
+Author URI: https://yourwebsite.com/
+License: GPL2
+*/
